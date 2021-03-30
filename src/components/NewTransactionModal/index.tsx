0ace941efc3c3e49ce16f55a8b7f11b8e869a42f
Thiago Mendes <thiagoromendes@gmail.com>
2021-03-30 16:56:42 -0300

    push complete project

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,101 @@
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import Modal from  'react-modal';
+import { FormEvent, useState } from "react";
+import { useTransactions } from '../../hooks/useTransactions';
+
+interface NewTransactionModalProps{
+    isOpen: boolean;
+    onRequestClose: () => void
+}
+
+export function NewTransactionModal({isOpen, onRequestClose} : NewTransactionModalProps){
+    const [title, setTitle] = useState('');
+    const [category, setCategory] = useState('');
+    const [value, setValue] = useState(0);
+    const [type, setType] = useState('deposit');
+    const {createTransaction} = useTransactions();
+
+    async function handleCreateNewTransaction(event:FormEvent){
+        event.preventDefault();
+        await createTransaction({
+            title,
+            amount: value,
+            category,
+            type
+        });
+        
+        setTitle('');
+        setValue(0);
+        setCategory('');
+        setType('deposit');
+        onRequestClose();
+    }
+    
+    return (
+            <Modal 
+                isOpen={isOpen}
+                onRequestClose={onRequestClose}
+                overlayClassName="react-modal-overlay"
+                className="react-modal-content"
+            >
+                <button 
+                    type="button" 
+                    onClick={onRequestClose} 
+                    className="react-modal-close"
+                >
+                    <img src={closeImg} alt="Fechar"/>
+                </button>
+
+                <Container onSubmit={handleCreateNewTransaction}>
+                    <h2>Cadastrar Transação</h2>
+
+                    <input 
+                        placeholder="Título"
+                        value={title}
+                        onChange={event => setTitle(event.target.value)}
+                    />
+
+                    <input 
+                        type="number"
+                        placeholder="Valor"
+                        value={value}
+                        onChange={event => setValue(Number(event.target.value))}
+                    />
+
+                    <TransactionTypeContainer>
+                        <RadioBox 
+                            type="button"
+                            onClick={() => setType('deposit')}
+                            isActive={type === 'deposit'}
+                            activeColor="green"
+                        >
+                            <img src={incomeImg} alt="Entrada"/>
+                            <span>Entrada</span> 
+                        </RadioBox>
+                        <RadioBox 
+                            type="button"
+                            onClick={() => setType('withdraw')}
+                            isActive={type === 'withdraw'}
+                            activeColor="red"
+                        >
+                            <img src={outcomeImg} alt="Saída"/>
+                            <span>Saída</span>
+                        </RadioBox>
+                    </TransactionTypeContainer>
+
+                    <input 
+                        placeholder="Categoria"
+                        value={category}
+                        onChange={event => setCategory(event.target.value)}
+                    />
+
+                    <button type="submit">
+                        Cadastrar
+                    </button>
+                </Container>
+            </Modal>
+    )
+}
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,78 @@
+import React, { useContext } from 'react';
+import InComeImg from '../../assets/income.svg';
+import OutComeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+import { useTransactions } from '../../hooks/useTransactions';
+
+import { Container } from "./styles";
+
+export function Summary() {
+    const { transactions } = useTransactions();
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if(transaction.type === 'deposit') {
+            acc.depostis += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+
+        return acc;
+    },{
+        depostis: 0,
+        withdraws: 0,
+        total: 0
+    })
+
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={InComeImg} alt="Entradas"/>
+                </header>
+                <strong>
+                {new Intl.NumberFormat(
+                    'pt-BR', 
+                    {
+                        style: 'currency',
+                        currency: 'BRL',
+                    }).format(summary.depostis)
+                }
+                </strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={OutComeImg} alt="Saídas"/>
+                </header>
+                <strong>
+                    -
+                {new Intl.NumberFormat(
+                    'pt-BR', 
+                    {
+                        style: 'currency',
+                        currency: 'BRL',
+                    }).format(summary.withdraws)
+                }
+                </strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total"/>
+                </header>
+                <strong>
+                {new Intl.NumberFormat(
+                    'pt-BR', 
+                    {
+                        style: 'currency',
+                        currency: 'BRL',
+                    }).format(summary.total)
+                }
+                </strong>
+            </div>
+        </Container>
+    )
+}
